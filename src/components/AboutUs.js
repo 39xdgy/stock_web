6987f9f2f9161efd80bdc315767d65b99ab601fd
Vishal Manjunath <vmanjuna@stevens.edu>
2021-05-18 14:29:06 -0400

    aboutus

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+function AboutUs () {
+    return (
+        <div>
+            <Title>About Us</Title>
+        </div>
+    )
+};
+
+export default AboutUs;
